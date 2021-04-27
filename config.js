9b737b37c0e281369a31b1962df737e0807827da
Lucy Sanders <lucyhsanders@gmail.com>
2021-04-27 12:15:14 -0600

    changed config and node version

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 "use strict";
 exports.DATABASE_URL =
-  process.env.DATABASE_URL || "mongodb://lulu:ml1284@ds153841.mlab.com:53841/artistogram-api";
+  process.env.DATABASE_URL || "mongodb+srv://Lucy:Silsa1284mdb@cluster0.xm1ci.mongodb.net/artistogram-api?retryWrites=true&w=majority";
 exports.TEST_DATABASE_URL =
-  process.env.TEST_DATABASE_URL || "mongodb://lulu:ml1284@ds153841.mlab.com:53841/artistogram-api";
+  process.env.TEST_DATABASE_URL || "mongodb+srv://Lucy:Silsa1284mdb@cluster0.xm1ci.mongodb.net/artistogram-api?retryWrites=true&w=majority";
 exports.PORT = process.env.PORT || 8080;
 
 exports.JWT_SECRET = process.env.JWT_SECRET || "WEST_HAM_UNITED";
